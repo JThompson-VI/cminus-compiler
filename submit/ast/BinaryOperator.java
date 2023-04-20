b17c2de261d736d72fb889984181e78dd63ced97
Jacob Thompson <a01657923@usu.edu>
2023-04-19 22:57:11 -0600

    Finish test2

--- submit/ast/BinaryOperator.java
+++ submit/ast/BinaryOperator.java
@@ -4,6 +4,10 @@
  */
 package submit.ast;
 
+import submit.MIPSResult;
+import submit.RegisterAllocator;
+import submit.SymbolTable;
+
 /**
  *
  * @author edwajohn
@@ -32,4 +36,29 @@ public class BinaryOperator extends AbstractNode implements Expression {
     rhs.toCminus(builder, prefix);
   }
 
+  @Override
+  public MIPSResult toMIPS(StringBuilder code, StringBuilder data, SymbolTable symbolTable, RegisterAllocator regAllocator) {
+    // TODO: 4/19/23 currently only dealing with +, -, *, /
+    MIPSResult lhsMips = lhs.toMIPS(code, data, symbolTable, regAllocator);
+    MIPSResult rhsMips = rhs.toMIPS(code, data, symbolTable, regAllocator);
+    if (type == BinaryOperatorType.PLUS || type == BinaryOperatorType.MINUS) {
+      code.append(String.format("%s %s %s %s\n",
+              type == BinaryOperatorType.MINUS ? "sub" : "add",
+              lhsMips.getRegister(),
+              lhsMips.getRegister(),
+              rhsMips.getRegister()));
+      regAllocator.clear(rhsMips.getRegister());
+      return MIPSResult.createRegisterResult(lhsMips.getRegister(), VarType.INT);
+    } else if (type == BinaryOperatorType.DIVIDE || type == BinaryOperatorType.TIMES) {
+      code.append(String.format("%s %s %s\n",
+                      type == BinaryOperatorType.DIVIDE ? "div" : "mult",
+                      lhsMips.getRegister(),
+                      rhsMips.getRegister()))
+              .append(String.format("mflo %s\n", lhsMips.getRegister()));
+      regAllocator.clear(rhsMips.getRegister());
+      return MIPSResult.createRegisterResult(lhsMips.getRegister(), VarType.INT);
+    }
+    System.err.println("Need to implement more binary operators");
+    return super.toMIPS(code, data, symbolTable, regAllocator);
+  }
 }
--- submit/ast/ParenExpression.java
+++ submit/ast/ParenExpression.java
@@ -4,6 +4,10 @@
  */
 package submit.ast;
 
+import submit.MIPSResult;
+import submit.RegisterAllocator;
+import submit.SymbolTable;
+
 /**
  *
  * @author edwajohn
@@ -23,4 +27,10 @@ public class ParenExpression extends AbstractNode implements Expression {
     builder.append(")");
   }
 
+  @Override
+  public MIPSResult toMIPS(StringBuilder code, StringBuilder data, SymbolTable symbolTable, RegisterAllocator regAllocator) {
+    MIPSResult exprMips = expression.toMIPS(code, data, symbolTable, regAllocator);
+    // TODO: 4/19/23 potential bug I am assuming all paren expressions return register result
+    return MIPSResult.createRegisterResult(exprMips.getRegister(), exprMips.getType());
+  }
 }
