0aab13eeb6f349076a87da85ea002b98dc09ff61
Jacob Thompson <a01657923@usu.edu>
2023-04-25 15:12:28 -0600

    remov abstract node print statement

--- submit/ast/AbstractNode.java
+++ submit/ast/AbstractNode.java
@@ -7,8 +7,6 @@ import submit.SymbolTable;
 public abstract class AbstractNode {
     public MIPSResult toMIPS(StringBuilder code, StringBuilder data,
                       SymbolTable symbolTable, RegisterAllocator regAllocator) {
-        System.out.println("toMips " + this.getClass());
-
         return MIPSResult.createVoidResult();
     }
 }
